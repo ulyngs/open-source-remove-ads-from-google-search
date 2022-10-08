d33d457a93964f1fbd6e9d8a483bb677534033ce
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2022-10-08 08:12:05 +0100

    add checkbox to show the ads

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,60 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
+// content.js
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        // recommended videos, home page //
+        // on desktop
+        ads = document.querySelector('#tads');
+        adsMobile = document.querySelector('div[tab-identifier="FEwhat_to_watch"]');
+        
+        // check for visibility
+        if(request.method == "checkAds"){
+            if (adsMobile == null){
+                if (ads.style.display === "none") {
+                    sendResponse({text: "hidden", method: "checkAds"});
+                } else if (ads.style.display === "block") {
+                    sendResponse({text: "visible", method: "checkAds"});
+                } else {
+                    sendResponse({text: "hidden", method: "checkAds"});
+                }
+            } else {
+                if (adsMobile.style.display === "none") {
+                    sendResponse({text: "hidden", method: "checkAds"});
+                } else if (adsMobile.style.display === "block") {
+                    sendResponse({text: "visible", method: "checkAds"});
+                } else {
+                    sendResponse({text: "hidden", method: "checkAds"});
+                }
+            }
+        }
+        
+        // change visibility
+        if(request.method == "changeAds"){
+            if (adsMobile == null){
+                if (ads.style.display === "none") {
+                    ads.style.display = "block";
+                    sendResponse({text: "ads visible", method: "changeAds"});
+                } else if (ads.style.display === "block") {
+                    ads.style.display = "none";
+                    sendResponse({text: "ads hidden", method: "changeAds"});
+                } else {
+                    ads.style.display = "block";
+                    sendResponse({text: "ads visible", method: "changeAds"});
+                }
+            } else {
+                if (adsMobile.style.display === "none") {
+                    adsMobile.style.display = "block";
+                    sendResponse({text: "ads visible", method: "changeAds"});
+                } else if (adsMobile.style.display === "block") {
+                    adsMobile.style.display = "none";
+                    sendResponse({text: "ads hidden", method: "changeAds"});
+                } else {
+                    adsMobile.style.display = "block";
+                    sendResponse({text: "ads visible", method: "changeAds"});
+                }
+            }
+        }
+        
+    }
+);
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+
--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,44 @@
-console.log("Hello World!", browser);
+// popup.js
+// https://developer.chrome.com/docs/extensions/mv3/messaging/
+
+document.addEventListener('DOMContentLoaded', function() {
+    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var activeTab = tabs[0].url;
+        // if we're on YouTube, then set the status of the checkboxes correctly
+        if (activeTab.indexOf("://www.google.com/") > -1 ){
+            
+            chrome.tabs.sendMessage(tabs[0].id, {method: "checkAds"}, function(response) {
+                
+                var adsCheckbox = document.getElementById('adsToggle');
+                
+                if(response.method === "checkAds"){
+                    if(response.text === "visible"){
+                        adsCheckbox.checked = true;
+                    } else {
+                        adsCheckbox.checked = false;
+                    }
+                }
+            });
+        }
+    });
+    
+    // assign functions to the checkboxes
+    var adsCheckbox = document.getElementById('adsToggle');
+    
+    // make it hide/show on mac
+    adsCheckbox.addEventListener('click', function() {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {method: "changeAds"}, function(response) {
+                
+                if(response.method == "changeAds"){
+                    if(response.text === "ads visible"){
+                        console.log("Ads are visible")
+                    } else {
+                        console.log("Ads are hidden")
+                    }
+                }
+            });
+          });
+        }, false);
+}, false);
